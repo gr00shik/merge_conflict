3f1e3fd1ebf1630575fa80ea727233490105d4ff
Sergey <gr00shik@gmail.com>
2023-01-16 19:09:00 +0300

    person2

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,5 +1,11 @@
-package org.example;public class Main {
+package org.example;
+
+public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Hello my world!");
+        for (int i = 0; i < 2; i++) {
+            System.out.println("--------- finish ----------");
+
+        }
     }
 }
